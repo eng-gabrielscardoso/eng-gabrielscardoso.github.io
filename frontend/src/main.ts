5b3805b17159ac534be0d2be4d0550681f60982e
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-06 23:03:09 -0300

    feat: add dependencies
    add libs: animate.css, bulma and font-awesome

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './registerServiceWorker'
+
 import router from './router'
+
+import 'animate.css';
+import '../node_modules/bulma/css/bulma.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 createApp(App).use(router).mount('#app')
