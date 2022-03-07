931d6ce82be30eef42682f2b93a812686bba19de
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-06 23:22:26 -0300

    chore: clear unused pre installed components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </nav>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
 
 <style lang="scss">
