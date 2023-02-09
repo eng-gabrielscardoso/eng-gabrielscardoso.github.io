77f163b6c50f03c058a5fb17886cc404252e989a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-02-08 22:51:03 -0300

    style: better footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,12 @@ export default function Footer(props) {
   return (
     <footer className="h-screen flex justify-center items-center bg-slate-300 text-green-600 font-bold" data-scroll-section>
       <div data-scroll data-scroll-direction="vertical" data-scroll-speed="9">
-        <h2>&copy; Gabriel Santos Cardoso - {currentYear}</h2>
+        <div class="flex flex-col gap-2 justify-center items-center p-2">
+          <picture>
+            <img src="https://raw.githubusercontent.com/MicaelliMedeiros/micaellimedeiros/master/image/computer-illustration.png" title="Computer banner" alt="Computer Banner" />
+          </picture>
+          <h2>&copy; Gabriel Santos Cardoso - {currentYear}</h2>
+        </div>
       </div>
     </footer>
   );
