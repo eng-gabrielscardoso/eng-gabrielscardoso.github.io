37f8c5bf8f07e1880ec08690f9269d9ce46427f5
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-07-08 13:42:07 -0300

    final configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal Portfolio
+
+This project is a personal portfolio for [Gabriel Santos Cardoso](https://linkedin.com/in/eng-gabrielscardoso).
+
+Feel free to contribute or create your own personal portfolio using this project as template.
+
+## Rights
+
+This project as licenced under the MIT Licence and you can read [HERE](LICENSE.md).
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-import svgr from 'vite-plugin-svgr';
-import tsConfigPath from 'vite-tsconfig-paths'
-import tailwindcss from 'tailwindcss'
-const autoprefixer = require('autoprefixer')
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
+import tsConfigPath from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tsConfigPath(), checker({ typescript: true })],
+  server: {
+    cors: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": "/src/",
     },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer,
-        tailwindcss
-      ]
-    }
   },
 });
