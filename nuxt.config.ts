f19c5d87883a73b345cc4e13f206d076a5ad462c
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-05-02 16:15:23 -0300

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: process.env.NODE_ENV === 'development' },
+
+  modules: [
+    '@pinia/nuxt',
+  ]
 })
