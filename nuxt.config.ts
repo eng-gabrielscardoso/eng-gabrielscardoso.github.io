267bcb7965f80fd290cd9f2a2d4db9bfb66979de
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-05-02 16:46:34 -0300

    feat: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV === 'development' },
 
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/i18n']
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt',
+  ]
 })
